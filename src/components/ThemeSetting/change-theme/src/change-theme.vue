67501e4dfdb31865474d82ed847de8379ecf5c56
xiaobinhong <xiaobinhong@xiaoe-tech.com>
2024-04-25 17:38:12 +0800

    feat: 主题切换持久化保存

--- src/components/ThemeSetting/change-theme/src/change-theme.vue
+++ src/components/ThemeSetting/change-theme/src/change-theme.vue
@@ -31,6 +31,7 @@ onMounted(async () => {
 })
 
 const predefineColors = ref([
+  'rgb(0,166,167)',
   '#ff4500',
   '#ff8c00',
   '#ffd700',
@@ -40,11 +41,6 @@ const predefineColors = ref([
   '#c71585',
   'rgba(255, 69, 0, 0.68)',
   'rgb(255, 120, 0)',
-  'hsv(51, 100, 98)',
-  'hsva(120, 40, 94, 0.5)',
-  'hsl(181, 100%, 37%)',
-  'hsla(209, 100%, 56%, 0.73)',
-  '#c7158577',
 ])
 // 主题颜色
 const changeTheme = async (data: number, type: number) => {
